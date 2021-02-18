61b18a367cdbbddebeeb57ac89df3c22fa250260
Yafik <mhmmdyfik@gmail.com>
2021-02-18 11:01:45 +0700

    Update Anggota, and Library Datatables

--- resources/views/pages/anggota.blade.php
+++ resources/views/pages/anggota.blade.php
@@ -13,170 +13,97 @@
 <div class="container-fluid mt-5">
     @csrf
     <div class="card">
-        <div class="card-header">
-            <a href="#" class="btn btn-success mr-3"><i class="ph-user-plus-bold"></i> Tambah Data</a>
-            <a href="#" class="btn btn-info"><i class="ph-pedestrian-bold"></i> Data Subject</a>
+        <div class="row mt-3 ml-3">
+            <div class="col-10">
+                <a href="#" class="btn btn-success mr-3"><i class="ph-user-plus-bold"></i> Tambah Data</a>
+                <a href="#" class="btn btn-info"><i class="ph-pedestrian-bold"></i> Data Subject</a>
+            </div>
+            <div class="col">
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="ph-download-bold"></i> Export Data
+                    </button>
+                    <div class="dropdown-menu" x-placement="bottom-start">
+                        <a class="dropdown-item has-icon" href="#"><i class="far fa-file-pdf"></i> Export to PDF</a>
+                        <a class="dropdown-item has-icon" href="#"><i class="far fa-file-excel"></i> Export to Excel</a>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="card-body">
-            <table id="example" class="table table-striped" style="width:100%">
-                <thead>
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">ID Card</th>
-                        <th scope="col">ID Chat</th>
-                        <th scope="col">No. Induk</th>
-                        <th scope="col">L/P</th>
-                        <th scope="col">Kelas</th>
-                        <th scope="col">Terdaftar</th>
-                        <th scope="col">Opsi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>6969</td>
-                        <td>6666</td>
-                        <td>Jeff</td>
-                        <td>Laki - laki</td>
-                        <td>Bisnis</td>
-                        <td>20 Maret 1998</td>
-                        <td>
-                            <a href="#" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rekaman"><i class="ph-clipboard-bold"></i></i></a>
-                            <a href="#" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><i class="ph-note-pencil-bold"></i></a>
-                            <a href="#" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><i class="ph-trash-simple-bold"></i></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>6969</td>
-                        <td>6666</td>
-                        <td>Jeff</td>
-                        <td>Laki - laki</td>
-                        <td>Bisnis</td>
-                        <td>20 Maret 1998</td>
-                        <td>
-                            <a href="#" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rekaman"><i class="ph-clipboard-bold"></i></i></a>
-                            <a href="#" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><i class="ph-note-pencil-bold"></i></a>
-                            <a href="#" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><i class="ph-trash-simple-bold"></i></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>6969</td>
-                        <td>6666</td>
-                        <td>Jeff</td>
-                        <td>Laki - laki</td>
-                        <td>Bisnis</td>
-                        <td>20 Maret 1998</td>
-                        <td>
-                            <a href="#" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rekaman"><i class="ph-clipboard-bold"></i></i></a>
-                            <a href="#" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><i class="ph-note-pencil-bold"></i></a>
-                            <a href="#" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><i class="ph-trash-simple-bold"></i></a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table id="example" class="table table-striped dataTable no-footer" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">ID Card</th>
+                            <th scope="col">ID Chat</th>
+                            <th scope="col">No. Induk</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">L/P</th>
+                            <th scope="col">Kelas</th>
+                            <th scope="col">Terdaftar</th>
+                            <th scope="col">Opsi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>6969</td>
+                            <td>6666</td>
+                            <td>6666</td>
+                            <td>Jeff</td>
+                            <td>Laki - laki</td>
+                            <td>Bisnis</td>
+                            <td>20 Maret 1998</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rekaman"><i class="ph-clipboard-bold"></i></i></a>
+                                <a href="#" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><i class="ph-note-pencil-bold"></i></a>
+                                <a href="#" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><i class="ph-trash-simple-bold"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>6969</td>
+                            <td>6666</td>
+                            <td>6666</td>
+                            <td>Naz</td>
+                            <td>Laki - laki</td>
+                            <td>Bisnis</td>
+                            <td>20 Maret 1998</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rekaman"><i class="ph-clipboard-bold"></i></i></a>
+                                <a href="#" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><i class="ph-note-pencil-bold"></i></a>
+                                <a href="#" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><i class="ph-trash-simple-bold"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>6969</td>
+                            <td>6666</td>
+                            <td>6666</td>
+                            <td>Zelda</td>
+                            <td>Laki - laki</td>
+                            <td>Bisnis</td>
+                            <td>20 Maret 1998</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rekaman"><i class="ph-clipboard-bold"></i></i></a>
+                                <a href="#" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><i class="ph-note-pencil-bold"></i></a>
+                                <a href="#" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><i class="ph-trash-simple-bold"></i></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     </div>
 </div>
 
-<!-- <div class="card">
-        <div class="row mt-3">
-            <div class="col mx-4">
-                <div class="d-flex justify-content-start">
-                    <a href="#" class="btn btn-success mr-3"><i class="ph-user-plus-bold"></i> Tambah Data</a>
-                    <a href="#" class="btn btn-info"><i class="ph-pedestrian-bold"></i> Data Subject</a>
-                </div>
-            </div>
-            <div class="col mx-4">
-                <div class="d-flex justify-content-end">
-                    <form class="card-header-form">
-                        <div class="input-group">
-                            <input type="text" name="search" class="form-control" placeholder="Search">
-                            <div class="input-group-btn">
-                                <button class="btn btn-primary btn-icon"><i class="fas fa-search"></i></button>
-                            </div>
-                        </div>
-                    </form>
-                    <div class="dropdown">
-                        <a href="#" data-toggle="dropdown" class="btn btn-warning dropdown-toggle" aria-expanded="false">Export Data</a>
-                        <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 25px, 0px); top: 0px; left: 0px; will-change: transform;">
-                            <a href="#" class="dropdown-item has-icon"><i class="fas fa-eye"></i> View</a>
-                            <a href="#" class="dropdown-item has-icon"><i class="far fa-edit"></i> Edit</a>
-                            <div class="dropdown-divider"></div>
-                            <a href="#" class="dropdown-item has-icon text-danger"><i class="far fa-trash-alt"></i> Delete</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-
-
-
-<!-- <div class="card-body">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">ID Card</th>
-                        <th scope="col">ID Chat</th>
-                        <th scope="col">No. Induk</th>
-                        <th scope="col">L/P</th>
-                        <th scope="col">Kelas</th>
-                        <th scope="col">Terdaftar</th>
-                        <th scope="col">Opsi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>6969</td>
-                        <td>6666</td>
-                        <td>Jeff</td>
-                        <td>Laki - laki</td>
-                        <td>Bisnis</td>
-                        <td>20 Maret 1998</td>
-                        <td>
-                            <a href="#" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rekaman"><i class="ph-clipboard-bold"></i></i></a>
-                            <a href="#" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><i class="ph-note-pencil-bold"></i></a>
-                            <a href="#" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><i class="ph-trash-simple-bold"></i></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>6969</td>
-                        <td>6666</td>
-                        <td>Jeff</td>
-                        <td>Laki - laki</td>
-                        <td>Bisnis</td>
-                        <td>20 Maret 1998</td>
-                        <td>
-                            <a href="#" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rekaman"><i class="ph-clipboard-bold"></i></i></a>
-                            <a href="#" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><i class="ph-note-pencil-bold"></i></a>
-                            <a href="#" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><i class="ph-trash-simple-bold"></i></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>6969</td>
-                        <td>6666</td>
-                        <td>Jeff</td>
-                        <td>Laki - laki</td>
-                        <td>Bisnis</td>
-                        <td>20 Maret 1998</td>
-                        <td>
-                            <a href="#" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rekaman"><i class="ph-clipboard-bold"></i></i></a>
-                            <a href="#" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><i class="ph-note-pencil-bold"></i></a>
-                            <a href="#" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><i class="ph-trash-simple-bold"></i></a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div> -->
-
 @endsection
 @push('js')
-<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.js"></script>
+<script type="text/javascript" src="  https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js  "></script>
+<script type="text/javascript" src=" https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js  "></script>
 <script>
     $(document).ready(function() {
         $('#example').DataTable();
@@ -184,5 +111,5 @@
 </script>
 @endpush
 @push('css')
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.css" />
+<link rel="stylesheet" type="text/css" href=" https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css  " />
 @endpush
--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('title')</title>
 
     <!-- General CSS Files -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <script src="https://unpkg.com/phosphor-icons"></script>
 
@@ -53,7 +53,7 @@
     </div>
 
     <!-- General JS Scripts -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
